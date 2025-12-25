db0e629aaab0e15d12f035a69db69a016c54a959
VishnuReddy3011 <sajjalavishnuvardhanreddy2001@gmail.com>
2025-12-25 10:52:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const userRoutes = require("./routes/userRoutes");
+app.use("/users", userRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
