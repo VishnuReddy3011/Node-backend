c3a16cd4c339e744add3007c058340244a1e89d8
VishnuReddy3011 <sajjalavishnuvardhanreddy2001@gmail.com>
2026-01-08 23:28:51 +0530

    config directory issue

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const ENV = require("../../config/env");
+const ENV = require("../config/env");
 
 const credentials = {
   dbAuthUsername: ENV.DB_AUTH_USERNAME,
